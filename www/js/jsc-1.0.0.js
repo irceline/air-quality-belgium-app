76bf0ef0dfcd9cf7321ad9172c42162e1e74dfcc
Olav Peeters <peeters@irceline.be>
2015-07-29 23:19:31 +0200

    'touchend' back to 'click'

--- www/js/jsc-1.0.0.js
+++ www/js/jsc-1.0.0.js
@@ -8657,7 +8657,7 @@ var Map = {
                         icon: ircelineIcon
                     });
                     /* end customIRCELINE */
-                    marker.on('touchend', $.proxy(that.markerClicked, that)); // customIRCELINE
+                    marker.on('click', $.proxy(that.markerClicked, that)); // customIRCELINE
                     this.stationMarkers.addLayer(marker);
                 }
             }, this));
